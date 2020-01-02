7ad5a6b9078c71f44b0dac0d74343c774eb5abf8
go.d.eye.0505 <go.d.eye.0505@gmail.com>
2020-01-03 01:52:05 +0900

    bundle exec rails generate controller dashboard index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'dashboard/index'
   resources :documents
   resources :news_categories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
