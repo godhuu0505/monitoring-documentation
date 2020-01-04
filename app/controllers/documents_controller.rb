1b3059e82119e3c21bedc9a6fed4cade04250ce2
go.d.eye.0505 <go.d.eye.0505@gmail.com>
2020-01-04 17:21:17 +0900

    shown document_data on document page

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -2,6 +2,7 @@ require 'open-uri'
 
 class DocumentsController < ApplicationController
   before_action :set_document, only: [:show, :download, :edit, :update, :destroy]
+  before_action :set_document_data, only: [:show]
 
   def index
     @documents = Document.all
@@ -17,6 +18,13 @@ class DocumentsController < ApplicationController
       open(filename, "w+b") do |out|
         out.write(doc.read)
       end
+
+      @document_datum = @document.document_datum.create(file_name: filename, document_type: "pdf")
+    end
+
+    respond_to do |format|
+      format.html { redirect_to @document, notice: 'Document was successfully downloaded.' }
+      format.json { head :no_content }
     end
   end
 
@@ -64,6 +72,10 @@ class DocumentsController < ApplicationController
   private
     def set_document
       @document = Document.find(params[:id])
+    end
+
+    def set_document_data
+      @document_data = @document.document_datum.all
     end
 
     def document_params
